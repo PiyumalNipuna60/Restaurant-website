07f5d28579c88b1660bd9315b98a99b524be408d
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-08-15 01:08:10 +0530

    frontEnd-add nav bar and create homepage

--- Front-End/webPage/src/app/app.module.ts
+++ Front-End/webPage/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginPageComponent,
+    HomePageComponent
   ],
   imports: [
     BrowserModule,
