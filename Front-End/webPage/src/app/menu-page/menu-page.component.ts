a9690342c093c1d9b059da26ddcd8dfccc433a71
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-08-19 13:48:25 +0530

    frontEnd-update menu page

--- Front-End/webPage/src/app/menu-page/menu-page.component.ts
+++ Front-End/webPage/src/app/menu-page/menu-page.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-menu-page',
   templateUrl: './menu-page.component.html',
   styleUrls: ['./menu-page.component.css']
 })
+
 export class MenuPageComponent {
+  // menuArray: any[] = [];
+  // isResultLoaded: boolean = false;
+
+  // imageUrl: string = "";
+  // header: string = "";
+  // deccription: string = "";
+  // price: number = 0;
+  // currentMenuId = "";
+
+  // constructor(private http: HttpClient) {
+  //   this.getAllMenuItems();
+  // }
+
+  // getAllMenuItems() {
+  //   this.http.get("")
+  //   .subscribe((resultData: any) => {
+  //       this.isResultLoaded = true;
+  //       console.log(resultData);
+  //       this.menuArray = resultData;
+  //     });
+  // }
 
 }
