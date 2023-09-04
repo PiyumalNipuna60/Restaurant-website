db1446052bc65c8111e2e18ca7c7c28a43fa8a99
PiyumalNipuna60 <nipuna315np@gmail.com>
2023-09-05 00:26:27 +0530

    front end : add Item save and getAll method

--- Front-End/webPage/src/app/item-form/item-form.component.ts
+++ Front-End/webPage/src/app/item-form/item-form.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,53 @@ import { Component } from '@angular/core';
 })
 export class ItemFormComponent {
 
+  ItemArrays: any[] = [];
+
+  itemId: string = "";
+  itemName: string = "";
+  qty: number = 0;
+  price: string = "";
+
+
+  constructor(private http: HttpClient) {
+    this.getAllItem();
+  }
+
+
+  getAllItem() {
+    this.http.get("http://localhost:8080/api/v1/item/get")
+      .subscribe((responseData: any) => {
+        console.log(responseData);
+        this.ItemArrays = responseData;
+      });
+  }
+
+
+  saveItem() {
+    let bodyData = {
+      "itemId": this.itemId,
+      "itemName": this.itemName,
+      "qty": this.qty,
+      "price": this.price
+    }
+
+    this.http.post("http://localhost:8080/api/v1/item/save", bodyData, { responseType: "text" })
+      .subscribe((responseData: any) => {
+        console.log(responseData);
+        alert("Item saved successfully..!");
+        this.getAllItem();
+        this.clear();
+      });
+  }
+
+
+  clear() {
+    this.itemId = "";
+    this.itemName = "";
+    this.qty = 0;
+    this.price = "";
+  }
+
+
+  
 }
